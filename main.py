eb20680084eb2c10afc668cfb4951f471c11049c
Jefferson Matheus <matheus.jefferson@escolar.ifrn.edu.br>
2026-04-07 09:06:51 -0300

    :sparkles: feat: Entendendo e Implementando o recurso query parameter

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
+
+# Query Parameter - Exemplo
+@app.get("/api/{categories}")
+def read_categories(categories):
+    return [{"name_categorie": f"{categories}", "dismissal": 130}]
+
+# Query Parameter - Exemplo 2
+@app.get("/api/dismissal/{month}")
+def read_month_dismissals(month):
+    dismissals = [random.randint(50, 450) for _ in range(3)]
+    return {"name_month" : f"{month}", "dismissals": dismissals}
